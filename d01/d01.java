db2c73387eb195a42a5b414064cc99f403c037cb
Martin <logout@bordeaux..>
2024-12-02 09:06:35 +0100

    First day of AoC 2024

---
+++ d01/d01.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+class d01 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer>array1, array2;
+        
+        array1 = new ArrayList<Integer>();
+        array2 = new ArrayList<Integer>();
+        
+        try (	
+                BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        ) {
+                String line = reader.readLine();
+                while(line != null) {
+                    String[] parts = line.split("   ");
+                    line = reader.readLine();
+                    array1.add(Integer.parseInt(parts[0]));
+                    array2.add(Integer.parseInt(parts[1]));      
+                    }
+                reader.close();
+        } catch (Exception ex) {
+                ex.printStackTrace();
+        }
+        
+        array1.sort(null);
+        array2.sort(null);
+        
+        int diff=0;
+        int similarity=0;
+        
+        for(int i=0;i<array1.size();i++) {
+            diff += Math.abs(array2.get(i)-array1.get(i));
+            similarity += array1.get(i) * Collections.frequency(array2, array1.get(i));
+        }
+        
+        System.out.print("Diff: ");
+        System.out.print(diff);
+        System.out.print("\t|\t");
+        System.out.print("Similarity: ");
+        System.out.println(similarity);
+    }
+}
