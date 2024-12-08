ef3099e38c7a3ef53533fa8be7d70776a58a95aa
Martin <logout128@gmail.com>
2024-12-08 15:55:56 +0100

    Day seven of AoC 2024

--- d07/d07.java
+++ d07/d07.java
@@ -3,7 +3,6 @@ import java.util.*;
 import java.lang.Math;
 
 class d07 {
-
     public static void main(String[] args) {
         ArrayList<Long> results;
         ArrayList<ArrayList<Integer>> numbers;
@@ -12,7 +11,7 @@ class d07 {
         numbers = new ArrayList();
         
         try (	
-                BufferedReader reader = new BufferedReader(new FileReader("sample.txt"));
+                BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
         ) {
                 String line = reader.readLine();
                 while(line != null) {
@@ -35,34 +34,51 @@ class d07 {
         long total = 0L;
 
         for(int i=0;i<results.size();i++) {
-            
-            System.out.println(results.get(i));
-            System.out.println(numbers.get(i));
-            
-            int digits = (numbers.get(i).size()-1);
-            
+            int digits = (numbers.get(i).size()-1);            
             for(int b=0;b<Math.pow(2,digits); b++) {        
                 String binNum = String.format(Character.toString(37)+Integer.toString(digits)+"s", Integer.toBinaryString(b)).replace(' ', '0');            
                 long current = 0L;
-                current += numbers.get(i).get(0);
-                System.out.print(current);
-                
+                current += numbers.get(i).get(0);                
                 for(int x=1; x<digits+1; x++) {
                     if (binNum.charAt(x-1)=='0') {
                         current += numbers.get(i).get(x);
-                        System.out.print("+");
                     } else {
                         current *= numbers.get(i).get(x);
-                        System.out.print("*");
                     }            
-                    System.out.print(numbers.get(i).get(x));                
-                }
-                
-                System.out.println("="+current +", "+ results.get(i));
-
-                System.out.println(current==results.get(i));
-                
+                }                
+                if (current==results.get(i)) {
+                    total += results.get(i);
+                    break;
+                }                
             }
         }
-    }
+        System.out.print("Binary total: ");
+        System.out.println(total);
+        
+        total = 0L;
+        for(int i=0;i<results.size();i++) {
+            int digits = (numbers.get(i).size()-1);
+            for(int b=0;b<Math.pow(3,digits); b++) {        
+                String triNum = String.format(Character.toString(37)+Integer.toString(digits)+"s", Integer.toString(b,3)).replace(' ', '0');
+                long current = 0L;
+                current += numbers.get(i).get(0);                
+                for(int x=1; x<digits+1; x++) {
+                    if (triNum.charAt(x-1)=='0') {
+                        current += numbers.get(i).get(x);
+                    } else if (triNum.charAt(x-1) == '1') {
+                        current *= numbers.get(i).get(x);
+                    } else {
+                        int exn = (int) Math.log10(numbers.get(i).get(x));
+                        current = current * (long) Math.pow(10, exn+1) + numbers.get(i).get(x);
+                    }
+                }                
+                if (current==results.get(i)) {
+                    total += results.get(i);
+                    break;
+                }
+            }
+        }        
+        System.out.print("Trinary total: ");
+        System.out.println(total);           
+    }    
 }
