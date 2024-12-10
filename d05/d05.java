59d58bc1b347f1f13bbe281a0faefd2cb98880fd
Martin <logout@bordeaux..>
2024-12-10 23:07:41 +0100

    Part two started, unfinishe

--- d05/d05.java
+++ d05/d05.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+
 class d05 {
 
     public static int getMiddleMember (ArrayList<Integer> list) {
@@ -8,18 +9,18 @@ class d05 {
         return list.get(middle);
     }
     
-    public static boolean validateNumber (ArrayList<ArrayList<Integer>> rules, ArrayList valueLine, int index) {
+    public static int validateNumber (ArrayList<ArrayList<Integer>> rules, ArrayList valueLine, int index) {
     
         for (int r=0; r<rules.size(); r++) {
             if(valueLine.get(index)==rules.get(r).get(0)) {
                 for (int i=0;i<index+1;i++) {
                     if (valueLine.get(i)==rules.get(r).get(1)) {
-                        return false;
+                        return i;
                     }
                 }
             }
         }    
-        return true;
+        return -1;
     }    
 
 
@@ -29,7 +30,7 @@ class d05 {
         ArrayList<ArrayList<Integer>> values = new ArrayList();
 
         try (	
-                BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+                BufferedReader reader = new BufferedReader(new FileReader("sample.txt"));
         ) {
                 String line;
                 while((line = reader.readLine()) != null) {                
@@ -56,27 +57,46 @@ class d05 {
                 ex.printStackTrace();
         }
 
-    int cResult = 0;
-    int iResult = 0;
-    boolean addIt=false;
+        int cResult = 0;
+        int iResult = 0;
+        int addIt=0;
 
-    for (ArrayList<Integer> valueLine : values) {    
-        
-        for (int v=1; v<valueLine.size(); v++) {
-            addIt = validateNumber(rules, valueLine, v);
-            if (addIt==false) {
-                break;
-            }
-        }
+        ArrayList<ArrayList<Integer>> incorrect = new ArrayList();
 
-        if (addIt==true) {
-            cResult += getMiddleMember(valueLine);
-        }
+        for (ArrayList<Integer> valueLine : values) {    
             
-    }
+            for (int v=1; v<valueLine.size(); v++) {
+                addIt = validateNumber(rules, valueLine, v);
+                if (addIt>=0) {
+                    incorrect.add(valueLine);
+                    break;
+                }
+            }
 
-    System.out.print("Part one result:");        
-    System.out.println(cResult);        
-    
+            if (addIt<0) {
+                cResult += getMiddleMember(valueLine);
+            }
+                
+        }
+
+        System.out.print("Part one result:");        
+        System.out.println(cResult);        
+        
+        for (ArrayList<Integer> valueLine : incorrect) {    
+            for (int v=1; v<valueLine.size(); v++) {            
+                int f = validateNumber(rules, valueLine, v);
+                
+                while (f>=0) {
+                    Collections.swap(valueLine, v, f);
+                    f = validateNumber(rules, valueLine, v);                                        
+                }
+                
+                iResult += getMiddleMember(valueLine);
+            }    
+        }
+
+        System.out.print("Part two result:");        
+        System.out.println(iResult);        
+
     }    
 }
