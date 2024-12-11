b6ad2862c2235f5541155f90353e629eeb298bcb
Martin <logout128@gmail.com>
2024-12-11 08:55:04 +0100

    Day one of AoC 2024

--- d01/d01.java
+++ d01/d01.java
@@ -9,6 +9,7 @@ class d01 {
         array1 = new ArrayList<Integer>();
         array2 = new ArrayList<Integer>();
         
+        // read and process data into lists
         try (	
                 BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
         ) {
@@ -23,13 +24,14 @@ class d01 {
         } catch (Exception ex) {
                 ex.printStackTrace();
         }
-        
+        // sort 'em        
         array1.sort(null);
         array2.sort(null);
         
         int diff=0;
         int similarity=0;
         
+        // iterate and calculate similarity and difference        
         for(int i=0;i<array1.size();i++) {
             diff += Math.abs(array2.get(i)-array1.get(i));
             similarity += array1.get(i) * Collections.frequency(array2, array1.get(i));
